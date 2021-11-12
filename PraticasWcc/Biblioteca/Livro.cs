87bddb18c0f2e1799b0be0c0804de51183ec1d7b
Marise de Amorim <marise.amorim@linx.com.br>
2021-11-11 21:57:38 -0300

    separacao de classes

--- PraticasWcc/Biblioteca/Livro.cs
+++ PraticasWcc/Biblioteca/Livro.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,19 +9,37 @@ namespace Biblioteca
 {
     class Livro
     {
-        public Livro(string titulo, string descricao, double preco, Autor autor, string genero)
+        public Livro()
+        {
+           
+        }
+
+        public Livro(string titulo, string descricao, double preco, Autor autor, Genero genero)
         {
             Titulo = titulo;
             Descricao = descricao;
             Preco = preco;
             Autor = autor;
             Genero = genero;
+            
         }
 
-        public string Titulo { get; set; }
-        public string Descricao { get; set; }
-        public double Preco { get; set; }
-        public Autor Autor { get; set; }
-        public string Genero { get; set; }
+        public string Titulo { get; private set; }
+        public string Descricao { get; private set; }
+        public double Preco { get; private set; }
+        public Autor Autor { get;private  set; }
+        public Genero Genero { get; private set; }
+    }
+    public enum Genero
+    {
+        [Description("Ficção")]
+        F = 01,
+
+        [Description("Tecnologia")]
+        T = 02,
+
+        [Description("Outros")]
+        O = 03,
+
     }
 }
--- PraticasWcc/Biblioteca/Program.cs
+++ PraticasWcc/Biblioteca/Program.cs
@@ -8,19 +8,31 @@ namespace Biblioteca
         static void Main(string[] args)
         {
             var lista = new List<Livro>() { };
-            Autor jk = new Autor(" J. K. Rowling ");
-            Livro livro1 = new Livro(" Harry Potter ", " Livro sobre uma escola de bruxaria ", 5, jk, " Ficção ");
+         
 
-            Autor autor3 = new Autor(" Robert Cecil Martin ");
-            Livro livro2 = new Livro(" Código Limpo ", " Livro sobre tecnologia ", 6.50, autor3, " Tecnologia ");
+            
+            Livro livro1 = new Livro(" Harry Potter ", " Livro sobre uma escola de bruxaria ", 5, new Autor(" J. K. Rowling "), Genero.F);
 
-            Autor auto3 = new Autor(" Desconhecido ");
-            Livro livro3 = new Livro(" Domain Drive Desing ", " Livro sobre tecnologia ", 50.55, auto3, " Tecnologia ");
+           
+            Livro livro2 = new Livro(" Código Limpo ", " Livro sobre tecnologia ", 6.50, new Autor(" Robert Cecil Martin "), Genero.F);
+
+            Livro livro3 = new Livro(" Domain Drive Desing ", " Livro sobre tecnologia ", 50.55, new Autor(" Desconhecido "), Genero.O);
 
             lista.Add(livro1);
             lista.Add(livro2);
 
+            List<Livro> lista2 = new List<Livro>
+            {
+                livro2,
+                livro3
+            };
+
             foreach (var elemento in lista)
+            {
+                Console.WriteLine($"{elemento.Titulo},{elemento.Descricao}, {elemento.Autor.Nome}, {elemento.Preco}, {elemento.Genero}");
+
+            }
+            foreach (var elemento in lista2)
             {
                 Console.WriteLine($"{elemento.Titulo},{elemento.Descricao}, {elemento.Autor.Nome}, {elemento.Preco}, {elemento.Genero}");
 
