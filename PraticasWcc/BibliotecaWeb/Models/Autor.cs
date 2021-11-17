96053c572030d066e4296de280edf30e0e639132
Marise de Amorim <marise.amorim@linx.com.br>
2021-11-17 19:04:53 -0300

    evolução de projetos

--- PraticasWcc/BibliotecaWeb/Models/Autor.cs
+++ PraticasWcc/BibliotecaWeb/Models/Autor.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Biblioteca
+namespace BibliotecaWeb.Models
 {
     public class Autor
     {
--- PraticasWcc/BibliotecaWeb/Models/Livro.cs
+++ PraticasWcc/BibliotecaWeb/Models/Livro.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Biblioteca
+namespace BibliotecaWeb.Models
 {
     public class Livro
     {
